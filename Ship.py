0d94d93067d43cf9ee2d3ce689176e88cb5c25c7
Pery Vieira <peryvieira_@hotmail.com>
2022-03-16 12:20:44 -0300

    feat: model ship

---
+++ Ship.py
@@ -0,0 +1,7 @@
+class Ship(object):
+    def __init__(self, object_ship):
+        self.nome = object_ship[0]
+        self.bandeira = object_ship[1]
+        self.data = object_ship[10]
+        self.hora = object_ship[11]
+        self.berco = object_ship[14]
